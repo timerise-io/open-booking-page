0a765d2666b159a4fb7520e2762ea46f252c242d
Łukasz Kowalczyk <freelancer.lukasz.kowalczyk@gmail.com>
2022-10-06 11:39:59 +0200

    Update useServiceLang.ts

--- src/features/i18n/useServiceLang.ts
+++ src/features/i18n/useServiceLang.ts
@@ -3,18 +3,31 @@ import { useTranslation } from "react-i18next";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { langAtom } from "state/atoms/langAtom";
 import { serviceAtom } from "state/atoms/service";
+import { useLangParam } from "./useLangParam";
+
+const UITranslation = ["en", "pl", "uk"];
 
 export const useServiceLang = () => {
   const serviceLang = useRecoilValue(serviceAtom)?.project.defaultLocale;
   const setLang = useSetRecoilState(langAtom);
   const { i18n } = useTranslation();
+  const paramLang = useLangParam();
 
   useEffect(() => {
     const defaultLang = serviceLang?.split("-")?.[0] ?? "en";
-    if (defaultLang !== null && ["en", "pl", "uk"].includes(defaultLang)) {
+
+    const paramLangPart = paramLang?.split("-")?.[0];
+
+    if (paramLangPart !== undefined && UITranslation.includes(paramLangPart)) {
+      i18n.changeLanguage(paramLangPart);
+      setLang(paramLangPart);
+      return;
+    }
+
+    if (defaultLang !== null && UITranslation.includes(defaultLang)) {
       i18n.changeLanguage(defaultLang);
       setLang(defaultLang);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [serviceLang]);
+  }, [serviceLang, paramLang]);
 };
