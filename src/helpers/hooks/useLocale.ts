1bebe6774b0790fa9270bf346d5ff7f9dacaefe7
Mateusz Sowa <sowa@hush.ai>
2023-03-07 23:59:28 +0100

    Update useLocale.ts

--- src/helpers/hooks/useLocale.ts
+++ src/helpers/hooks/useLocale.ts
@@ -1,19 +1,38 @@
-import { enGB, pl, uk, de } from "date-fns/esm/locale";
+import { cs, nl, enGB, fi, fr, de, hu, pl, pt, es, sk, sv, uk } from "date-fns/esm/locale";
 import { useRecoilValue } from "recoil";
 import { dateLocaleSelector } from "state/selectors/dateLocale";
 
 export const useLocale = () => {
   const locale = useRecoilValue(dateLocaleSelector);
-
   switch (locale) {
+    case "cs":
+      return cs;
+    case "nl":
+      return nl;
     case "en-gb":
       return enGB;
-    case "pl":
-      return pl;
-    case "uk":
-      return uk;
+    case "fi":
+      return fi;
+    case "fr":
+      return fr;
     case "de":
       return de;
+    case "hu":
+      return hu;
+    case "it":
+      return it;
+    case "pl":
+      return pl;
+    case "pt":
+      return pt;
+    case "es":
+      return es;
+    case "sk":
+      return sk;
+    case "sv":
+      return sv;
+    case "uk":
+      return uk;
     default:
       return enGB;
   }
