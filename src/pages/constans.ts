9bd9fd72033052d985df36806a119fc69c9827c9
Daniel Dobkowski <daniel@Daniels-MacBook-Pro.local>
2023-06-28 11:40:26 +0200

    fix(Booking): fixed confirmation redirect

--- src/pages/constans.ts
+++ src/pages/constans.ts
@@ -2,7 +2,7 @@ export const PAGES = {
   SERVICE: "/service/:id/",
   SERVICES: "/services/:id/",
   BOOKING: "/booking/:id/",
-  BOOKING_CONFIRMATION: "/booking/:id/confirmation/",
+  BOOKING_CONFIRMATION: "/booking/:id/confirmation",
   TERMS: "/terms/",
   PRIVACY: "/privacy/",
   RESCHEDULE: "/reschedule/:id/",
