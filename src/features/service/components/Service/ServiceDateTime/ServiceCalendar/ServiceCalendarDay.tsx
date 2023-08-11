b118c3023551766a01b16917aec250fefba24b3b
Daniel Dobkowski <daniel@Daniels-MacBook-Pro.local>
2023-08-11 20:11:51 +0200

    fix(Booking): fixed render proper in calendar header

--- src/features/service/components/Service/ServiceDateTime/ServiceCalendar/ServiceCalendarDay.tsx
+++ src/features/service/components/Service/ServiceDateTime/ServiceCalendar/ServiceCalendarDay.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { Typography } from "components/Typography";
 import { Box } from "components/layout/Box";
 import { Column } from "components/layout/Column";
+import { formatInTimeZone } from "date-fns-tz";
 import { useLocale } from "helpers/hooks/useLocale";
-import { convertSourceDateTimeToTargetDateTime } from "helpers/timeFormat";
 import { useRecoilValue } from "recoil";
-import { serviceAtom } from "state/atoms/service";
-import { timeZoneAtom } from "state/atoms/timeZone";
 import { slotsDayPattern } from "state/selectors/slotsDayPattern";
 import styled from "styled-components";
 import TimeSlot from "./TimeSlot";
@@ -30,28 +28,18 @@ const ServiceCalendarDay: React.FC<ServiceCalendarDayProps> = ({ day }) => {
   const locale = useLocale();
   const dayPart = day.split("T")[0];
   const pattern = useRecoilValue(slotsDayPattern);
-  const timeZone = useRecoilValue(timeZoneAtom);
-  const service = useRecoilValue(serviceAtom)!;
 
   return (
     <ServiceCalendarDayWrapper>
       <Box mb={1.5}>
         <Typography typographyType="body" weight="bold" as="div" align="center">
-          {convertSourceDateTimeToTargetDateTime({
-            date: day,
-            targetTimeZone: timeZone,
-            sourceTimeZone: service.project.localTimeZone,
-            dateFormat: "iii",
-            locale,
+          {formatInTimeZone(day, "UTC", "iii", {
+            locale: locale,
           }).replace(/[.]$/, "")}
         </Typography>
         <DateTypography className="date-text" typographyType="body" as="div" align="center">
-          {convertSourceDateTimeToTargetDateTime({
-            date: day,
-            targetTimeZone: timeZone,
-            sourceTimeZone: service.project.localTimeZone,
-            dateFormat: "dd MMM",
-            locale,
+          {formatInTimeZone(day, "UTC", "dd MMM", {
+            locale: locale,
           })}
         </DateTypography>
       </Box>
