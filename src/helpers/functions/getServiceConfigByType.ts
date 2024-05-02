568fbc4e182c9ae735f841ea246234e7e6e6ad5e
Daniel Dobkowski <ddobkowski@trans.eu>
2024-05-02 10:12:09 +0200

    fix(Booking): fixed booking button text

--- src/helpers/functions/getServiceConfigByType.ts
+++ src/helpers/functions/getServiceConfigByType.ts
@@ -6,10 +6,11 @@ interface GetServiceConfigByType {
 
 export const getServiceConfigByType: GetServiceConfigByType = ({ service }) => {
   const serviceType = service?.viewConfig?.displayType;
+
   const types = {
     [BOOKING_FORM_TYPES.DAYS]: "days",
-    [BOOKING_FORM_TYPES.CALENDAR]: "list",
-    [BOOKING_FORM_TYPES.LIST]: "calendar",
+    [BOOKING_FORM_TYPES.CALENDAR]: "calendar",
+    [BOOKING_FORM_TYPES.LIST]: "list",
   } as const;
 
   return service.viewConfig[types[serviceType]];
