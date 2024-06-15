b719db3c1f1876f3a1f360af03b80bda5b18343c
Daniel Dobkowski <ddobkowski@trans.eu>
2024-06-15 14:25:08 +0200

    feat(HourSystem): get hour system from timezone locale

--- src/state/atoms/hoursSystem.ts
+++ src/state/atoms/hoursSystem.ts
@@ -1,7 +1,23 @@
 import { HOURS_SYSTEMS } from "features/service/components/Service/HoursSystem/enums/HoursSystem.enum";
 import { atom } from "recoil";
 
+const localeUses24HourTime = (langCode: string) => {
+  return (new Intl.DateTimeFormat(langCode, {
+    hour: "numeric",
+  })
+    .formatToParts(new Date(2024, 0, 1, 13))
+    .find((part) => part.type === "hour")?.value?.length === 2) as boolean;
+};
+
+const getHoursSystem = () => {
+  const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+  const hasLocalStorageValue = localStorage.getItem("HOURS_SYSTEM");
+  const uses24HourTime = localeUses24HourTime(locale);
+
+  return hasLocalStorageValue ?? (uses24HourTime ? HOURS_SYSTEMS.h24 : HOURS_SYSTEMS.h12);
+};
+
 export const hoursSystemAtom = atom({
   key: "hoursSystem",
-  default: localStorage.getItem("HOURS_SYSTEM") ?? HOURS_SYSTEMS.h24,
+  default: getHoursSystem(),
 });
